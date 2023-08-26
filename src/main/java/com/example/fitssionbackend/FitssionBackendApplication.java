8fdeba2dcdbc18bcb7a6b4f35fd57f4ad8143837
HunSeongPark <phunseong@gmail.com>
2023-08-26 15:00:02 +0900

    initial commit

---
+++ src/main/java/com/example/fitssionbackend/FitssionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fitssionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitssionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitssionBackendApplication.class, args);
+	}
+
+}
